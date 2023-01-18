12a60b95a06b0f3bd8e5534d3518f962d2eaf1c1
francis <cifren@msn.com>
2023-01-18 16:40:23 +0000

    Clean + install primeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {BadgeModule} from 'primeng/badge';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
